5d17f3e2850fadaf0cd3a05abe5bb43a2fa7f27c
08swa <08swa@96636880978660>
2025-04-05 15:59:59 +0530

    Fork and pull request added

---
+++ Seleniumm/src/SwathiShetty/TestSample.java
@@ -0,0 +1,13 @@
+package SwathiShetty;
+
+import org.testng.Reporter;
+
+public class TestSample {
+	
+	@org.testng.annotations.Test
+	
+	public void Test() {
+		Reporter.log("Fork and pull request added");
+	}
+
+}
